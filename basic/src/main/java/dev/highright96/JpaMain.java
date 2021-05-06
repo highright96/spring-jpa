08b671aa2335fa100b86984313fb84607779088b
Sangwoo96 <nsw5296@naver.com>
2021-05-06 17:30:55 +0900

    [Add] 불변 객체, 값 타입 비교

--- basic/src/main/java/dev/highright96/JpaMain.java
+++ basic/src/main/java/dev/highright96/JpaMain.java
@@ -21,10 +21,6 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Member member = new Member();
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
-            member.setPeriod(new Period());
-            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
@@ -35,6 +31,35 @@ public class JpaMain {
         }
 
         emf.close();
+    }
+
+    private static void ValueTypeEquals() {
+        Address address1 = new Address("city", "street", "zipcode");
+        Address address2 = new Address("city", "street", "zipcode");
+
+        System.out.println(address1 == address2);
+        System.out.println(address1.equals(address2));
+    }
+
+    private static void immutableType(EntityManager em) {
+        Address address = new Address("city", "street", "zipcode");
+        Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+        Member member1 = new Member();
+        member1.setName("1");
+        member1.setHomeAddress(address);
+        member1.setPeriod(new Period());
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setName("2");
+        //member2.setHomeAddress(address);
+        member2.setHomeAddress(copyAddress);
+        member2.setPeriod(new Period());
+        em.persist(member2);
+
+        //임베디드 타입(값 타입)을 member1, member2 엔티티에서 공유함 -> 공유 참조 부작용 발생 -> setter 를 없앰 -> copyAddress 를 사용
+        //member1.getHomeAddress().setCity("newCity");
     }
 
     private static void cascade(EntityManager em) {
